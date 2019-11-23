9729a733aba50a3c835341b9a1174bc25b8cd596
ikenwaa <ajohn9353@gmail.com>
2019-11-24 00:24:09 +0100

    Updates

---
+++ precache-manifest.f4ab8864d5a128ba77a69f07126daf28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d04aa1f469eef1a69ae63386e7c110",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "00608f6a5bc862808a38",
+    "url": "/monsters-rolodex/static/css/main.47b5e1be.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "00608f6a5bc862808a38",
+    "url": "/monsters-rolodex/static/js/main.13c3bcc8.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
